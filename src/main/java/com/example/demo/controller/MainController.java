38486c85d788a39f62cba8519e5872ef0a907ced
mirkamalice <mirkamali.ce@gmail.com>
2024-03-04 15:13:06 +0330

    add multiply,sub,divide

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -7,7 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
     @GetMapping("/calc/sum/{x}/{y}")
-    public String sendHello(@PathVariable int x ,@PathVariable int y){
+    public String sum(@PathVariable int x ,@PathVariable int y){
         return "x+y= "+ (x+y);
+    }
+    @GetMapping("/calc/sub/{x}/{y}")
+    public String sub(@PathVariable int x ,@PathVariable int y){
+        return "x-y= "+ (x-y);
+    }
+    @GetMapping("/calc/mul/{x}/{y}")
+    public String mult(@PathVariable int x ,@PathVariable int y){
+        return "x*y= "+ (x*y);
+    }
+    @GetMapping("/calc/div/{x}/{y}")
+    public String div(@PathVariable int x ,@PathVariable int y){
+        return "x/y= "+ (x/y);
     }
 }
